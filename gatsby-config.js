14220830102cccd1fafcf531c3132560ff2aec12
Jilvan Cândido <jilvan.silva@gmail.com>
2020-04-14 17:03:22 -0400

    💾 Fixed position gatsby-plugin-google-analytics in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,20 +8,29 @@ module.exports = {
     position: 'Full Stack Software Developer',
     aboutme: 'I\'m a full stack software developer specializing in modern JavaScript, I work with front-end or back-end to creating apps and web pages. Nowadays I work with React Native and I like a lot. I like study stuffs about technology and development, I\'m continue improve my Javascript stack, with Reactjs, React Native, Node.js.',
     skills: [
-      { name: 'NodeJS', level: 70 },
+      { name: 'NodeJS', level: 90 },
       { name: 'PHP', level: 80 },
       { name: 'GraphQL', level: 60 },
-      { name: 'RN', level: 65 },
-      { name: 'Apollo', level: 45 },
-      { name: 'Mysql', level: 85 },
-      { name: 'MongoDB', level: 50 },
+      { name: 'React Native', level: 65 },
+      { name: 'MySql', level: 85 },
+      { name: 'MongoDB', level: 70 },
       { name: 'ReactJS', level: 70 },
       { name: 'Laravel', level: 80 },
-      { name: 'AWS', level: 15 },
+      { name: 'Google Cloud', level: 40 },
+      { name: 'AWS', level: 40 },
       { name: 'Docker', level: 80 },
     ],
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: process.env.GATSBY_GA_TRACKING_ID,
+        head: true,
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-filesystem',
@@ -49,14 +58,6 @@ module.exports = {
     'gatsby-plugin-offline',
     'gatsby-plugin-sitemap',
     'gatsby-plugin-sass',
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: process.env.GATSBY_GA_TRACKING_ID,
-        head: false,
-        anonymize: true,
-        respectDNT: true,
-      },
-    },
+
   ],
 };
